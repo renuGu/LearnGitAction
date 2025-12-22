69610f9c711fd3a8d82ccbba841c29d805c60cff
Renu <bansal_renu@yahoo.com>
2025-12-21 19:28:19 -0800

    modified

--- .github/workflows/ascii-txt.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-Sudo apt-get install cowsay -y
-cowsay -f dragon “Run for cover, I am DRAGON” >> dragon.txt
-grep -I “dragon” dragon.txt
-cat dragon.txt
-ls -lrta
--- ascii.sh
+++ ascii.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-Sudo apt-get install cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f dragon “Run for cover, I am DRAGON” >> dragon.txt
 grep -I “dragon” dragon.txt
 cat dragon.txt
